9ae2c998e95094971f2d4d471ec47efd396c1c53
Gaurav Chadha <65453826+Chadha93@users.noreply.github.com>
2020-07-24 13:21:58 +0530

    update

---
+++ index.js
@@ -0,0 +1,11 @@
+function include(file) { 
+
+  var script = document.createElement('script'); 
+  script.src = file; 
+  script.type = 'text/javascript'; 
+  script.defer = true; 
+  document.getElementsByTagName('body').item(0).appendChild(script); 
+}
+include('grid/main.js');
+include('base/object.js'); 
+include('algorithms/dijkstra');
