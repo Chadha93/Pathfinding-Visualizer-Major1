07a0cc792f7bfd009f79c15a74fc3db3deb22a7c
Chadha93 <65453826+Chadha93@users.noreply.github.com>
2020-09-11 23:22:13 +0530

    auth updated

--- login.js
+++ login.js
@@ -24,7 +24,7 @@ var uiConfig = {
     // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
     firebase.auth.GithubAuthProvider.PROVIDER_ID,
     firebase.auth.EmailAuthProvider.PROVIDER_ID,
-    firebase.auth.PhoneAuthProvider.PROVIDER_ID
+    // firebase.auth.PhoneAuthProvider.PROVIDER_ID
   ],
   // Terms of service url.
   tosUrl: 'index.html',
